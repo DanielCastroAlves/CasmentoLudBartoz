126abc5d8192d92a937225f312dfbf8e362be036
MGodoySL <matheusgodoy.sl@gmail.com>
2023-08-14 17:53:36 -0300

    <fix> correção da altura do componente da home

--- src/domains/Home/style.js
+++ src/domains/Home/style.js
@@ -4,7 +4,7 @@ import bgImage from "../../Assets/Images/bgmain.jpg";
 const HomeContainer = styled(Stack)(() => ({
     // position: "absolute",
     width: "100%",
-    height: "1200px",
+    height: "100vh",
     backgroundImage: `linear-gradient(360deg, #FFF 0%, rgba(255, 255, 255, 0.78) 37.87%, rgba(255, 255, 255, 0.00) 81.77%), url(${bgImage})`,
     backgroundPosition: "center",
     backgroundSize: "cover",
