61638b325e30d707141468a9ec89422775c91223
MGodoySL <matheusgodoy.sl@gmail.com>
2023-10-14 17:49:58 -0300

    <fix> ajuste de layout e fontsize

--- src/domains/Wedding/components/LocationAndWeather/index.jsx
+++ src/domains/Wedding/components/LocationAndWeather/index.jsx
@@ -1,7 +1,7 @@
 import { Box, Stack, Typography, dividerClasses, useMediaQuery, useTheme } from "@mui/material";
 import { useLanguage } from "../../../../hooks/useLanguage";
 import { useEffect, useState } from "react";
-import { CeremonyContainer, CeremonyContainerMobile } from "./style";
+import { CeremonyContainer, CeremonyContainerMobile, brMapContainer } from "./style";
 import sargatana from "../../../../Assets/Images/Sargatana.png";
 import brMap from "../../../../Assets/Images/brMap.png";
 import text from "../../../../Assets/text.json";
@@ -28,6 +28,20 @@ export const LocationAndWeather = () => {
             document.body.removeChild(script);
         };
     }, [language]);
+
+    const isSmallScreen = useMediaQuery("(max-width:1500px)");
+    const isMediumScreen = useMediaQuery("(max-width:1600px)");
+
+    let fontSizeDressCode = theme.spacing(5.5);
+    let fontSizeDressCodeOption = theme.spacing(4);
+
+    if (isMediumScreen) {
+        fontSizeDressCode = theme.spacing(4.5);
+        fontSizeDressCodeOption = theme.spacing(3);
+    } else if (isSmallScreen) {
+        fontSizeDressCode = "1px";
+        fontSizeDressCodeOption = theme.spacing(2.5);
+    }
 
     return (
         <Stack width="100%" alignItems="center" px={!isMobile && 42.75} mt={!isMobile && 15}>
@@ -57,35 +71,49 @@ export const LocationAndWeather = () => {
                                     </>
                                 )}
                             </Stack>
-                            <Typography fontSize={theme.spacing(4.5)} fontWeight={300} color={theme.palette.gray['500']} fontFamily="Open Sans">{text.preWedding.dateTime[language]}</Typography>
+                            <Typography fontSize={theme.spacing(4.5)} fontWeight={300} color={theme.palette.gray["500"]} fontFamily="Open Sans">
+                                {text.preWedding.dateTime[language]}
+                            </Typography>
                         </Stack>
-                        <Stack direction="row" width="100%" height="100%" px={10} gap={4} my={5}>
-                            <Stack gap={4} width="100%">
-                                <Typography fontSize={theme.spacing(9.5)} color={theme.palette.secondary.main} fontFamily="Corinthia">
-                                    Villa Três Marias
-                                </Typography>
-                                <Stack>
-                                    <Typography color={theme.palette.gray["500"]} fontWeight={300} fontFamily="Open Sans">
-                                        {text.preWedding.addressP1[language]}
+                        <Stack direction="row" width="100%" height="100%" px={2} gap={4} my={5}>
+                            <Stack gap={4} width="50%" alignItems="center">
+                                <Stack width="100%" alignItems="center">
+                                    <Typography fontSize={theme.spacing(9.5)} color={theme.palette.secondary.main} fontFamily="Corinthia">
+                                        Villa Três Marias
                                     </Typography>
-                                    <Typography color={theme.palette.gray["500"]} fontWeight={300} fontFamily="Open Sans">
-                                        {text.preWedding.addressP2[language]}
-                                    </Typography>
+                                    <Stack>
+                                        <Typography color={theme.palette.gray["500"]} fontWeight={300} fontFamily="Open Sans">
+                                            {text.preWedding.addressP1[language]}
+                                        </Typography>
+                                        <Typography color={theme.palette.gray["500"]} fontWeight={300} fontFamily="Open Sans">
+                                            {text.preWedding.addressP2[language]}
+                                        </Typography>
+                                    </Stack>
                                 </Stack>
-                                <Box my={5}>
+                                <Stack direction="row" width="100%" justifyContent="center" my={5}>
                                     <img src={sargatana} alt="Lagartixa marron" />
-                                </Box>
-                                <Stack direction="row" width="100%" gap={4} alignItems="baseline">
-                                    <Typography fontSize={theme.spacing(5.5)} color={theme.palette.secondary.main} fontWeight={400}>
+                                </Stack>
+                                <Stack direction="row" width="100%" gap={4} justifyContent="center" alignItems="baseline">
+                                    <Typography fontSize={fontSizeDressCode} color={theme.palette.secondary.main} fontWeight={400}>
                                         {language === "es" ? "Código de vestimenta:" : "Dress Code:"}
                                     </Typography>
-                                    <Typography color={theme.palette.gray["500"]} fontWeight={300} fontFamily="Open Sans">
+                                    <Typography fontSize={fontSizeDressCodeOption} color={theme.palette.gray["500"]} fontWeight={300} fontFamily="Open Sans">
                                         {text.preWedding.dressCode[language]}
                                     </Typography>
                                 </Stack>
                             </Stack>
-                            <Stack>
-                                <img src={brMap} alt="Mapa geográfico do Brasil" />
+                            <Stack
+                                width="50%"
+                                sx={{
+                                    backgroundImage: `linear-gradient(360deg, #FFF 0%, rgba(255, 255, 255, 0.78) 1.87%, rgba(255, 255, 255, 0.00) 51.77%), url(${brMap})`,
+                                    backgroundPosition: "center",
+                                    backgroundSize: "contain", // A imagem será redimensionada para cobrir completamente o elemento pai sem distorções
+                                    backgroundRepeat: "no-repeat",
+                                    maxWidth: "-webkit-fill-available",
+                                    overflow: "auto !important",
+                                }}
+                            >
+                                {/* <img src={brMap} alt="Mapa geográfico do Brasil" /> */}
                             </Stack>
                         </Stack>
                     </CeremonyContainer>
@@ -143,7 +171,7 @@ export const LocationAndWeather = () => {
                         </Stack>
                     </CeremonyContainerMobile>
                 )}
-                <Box width={!isMobile ? "50%" : "100%"} height={isMobile ? "315px" : "auto"} px={isMobile && 4} mt={isMobile && 4} sx={{border: `1px solid ${theme.palette.primary.main}`}}>
+                <Box width={!isMobile ? "50%" : "100%"} height={isMobile ? "315px" : "auto"} px={isMobile && 4} mt={isMobile && 4} sx={{ border: `1px solid ${theme.palette.primary.main}` }}>
                     <iframe
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3678.946544808715!2d-41.91187708872573!3d-22.76736497926735!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x96ff1202fcdb65%3A0xa27673cadca1dc57!2sVilla%20Tres%20Marias%20Buzios!5e0!3m2!1sen!2sch!4v1689846300672!5m2!1sen!2sch"
                         width="100%"
@@ -156,7 +184,21 @@ export const LocationAndWeather = () => {
                 </Box>
             </Stack>
             <Stack width="100%" height="100%" mt={!isMobile ? 8 : 4} px={isMobile && 4}>
-                <a class="weatherwidget-io" href={urlLanguage} data-accent="" data-raincolor="#04405F" data-suncolor="#D4AE60" data-textcolor="#545454" data-font="Open Sans" data-label_1="BÚZIOS" data-label_2={text.Wedding.weatherLabel[language]} data-icons="Climacons Animated" data-theme="pure" data-highcolor="#9C4522" data-lowcolor="#04405F">
+                <a
+                    class="weatherwidget-io"
+                    href={urlLanguage}
+                    data-accent=""
+                    data-raincolor="#04405F"
+                    data-suncolor="#D4AE60"
+                    data-textcolor="#545454"
+                    data-font="Open Sans"
+                    data-label_1="BÚZIOS"
+                    data-label_2={text.Wedding.weatherLabel[language]}
+                    data-icons="Climacons Animated"
+                    data-theme="pure"
+                    data-highcolor="#9C4522"
+                    data-lowcolor="#04405F"
+                >
                     {text.Wedding.weatherLabel[language]}
                 </a>
             </Stack>
--- src/domains/Wedding/components/LocationAndWeather/style.js
+++ src/domains/Wedding/components/LocationAndWeather/style.js
@@ -1,20 +1,39 @@
 import { Stack, styled } from "@mui/material";
 import { Link } from "react-router-dom";
+import brMap from "../../../../Assets/Images/brMap.png";
 
-const CeremonyContainer = styled(Stack)(({theme}) => ({
-    alignItems: 'center',
+const CeremonyContainer = styled(Stack)(({ theme }) => ({
+    alignItems: "center",
     border: `1px solid ${theme.palette.primary.main}`,
-    direction: 'column',
+    direction: "column",
     gap: theme.spacing(8),
-    padding: `${theme.spacing(11)} ${theme.spacing(4)}`,
+    padding: `${theme.spacing(11)} ${theme.spacing(0)}`,
     width: "50%%",
 }));
-const CeremonyContainerMobile = styled(Stack)(({theme}) => ({
-    alignItems: 'center',
+
+const brMapContainer = styled(Stack)(({ theme }) => ({
+    // position: "absolute",
+    width: "300px",
+
+    height: "300px",
+    // backgroundImage: `linear-gradient(360deg, #FFF 0%, rgba(255, 255, 255, 0.78) 1.87%, rgba(255, 255, 255, 0.00) 51.77%), url(${brMap})`,
+    // backgroundPosition: "center",
+    // backgroundSize: "cover",
+    // backgroundRepeat: "no-repeat",
+    // maxWidth: "-webkit-fill-available",
+    // overflow: "auto !important",
+    backgroundColor: "red",
+
+    // [theme.breakpoints.down("sm")]: {
+    //     backgroundImage: `linear-gradient(360deg, #FFF 0%, rgba(255, 255, 255, 0.78) 1.87%, rgba(255, 255, 255, 0.00) 51.77%), url(${bgMobile})`,
+    // },
+}));
+const CeremonyContainerMobile = styled(Stack)(({ theme }) => ({
+    alignItems: "center",
     border: `1px solid ${theme.palette.primary.main}`,
-    direction: 'column',
+    direction: "column",
     margin: `${theme.spacing(8)} ${theme.spacing(4)} 0px ${theme.spacing(4)}`,
     padding: `${theme.spacing(11)} ${theme.spacing(4)}`,
 }));
 
-export { CeremonyContainer, CeremonyContainerMobile };
+export { CeremonyContainer, CeremonyContainerMobile, brMapContainer };
