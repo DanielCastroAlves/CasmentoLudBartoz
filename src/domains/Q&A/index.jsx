e120141b9057a4258ebb0ac271a717f8ec758491
Seu Nome de Usuário <danielvortex@hotmail.com>
2023-10-24 08:53:23 -0300

    ajuste no accordion

--- src/domains/Q&A/index.jsx
+++ src/domains/Q&A/index.jsx
@@ -8,6 +8,7 @@ import { FirstSection } from "../../Components/FirstSection/index.jsx";
 const QeA = () => {
   const { language } = useLanguage();
   const data = [
+    //1
     {
       Q: text.QEA.Accordion[0].questions[language],
       A: [
@@ -17,30 +18,35 @@ const QeA = () => {
         },
       ],
     },
+    //2
+    //tentar por italico
     {
       Q: text.QEA.Accordion[1].questions[language],
       A: [
         {
           id: 2,
-          lista: [
-            text.QEA.Accordion[1].answers[language],
-            text.QEA.Accordion[1].answers2[language],
+          description: [text.QEA.Accordion[1].answers[language]],
+        },
+        {
+          description: [
+            text.QEA.Accordion[1].answers3[language] +
+              " " +
+              text.QEA.Accordion[1].answers2[language],
           ],
         },
       ],
     },
+    //3
+    //tentar por negrito
     {
       Q: text.QEA.Accordion[2].questions[language],
       A: [
-        {
-          id: 3,
-          lista: [
-            text.QEA.Accordion[2].answers[language],
-            text.QEA.Accordion[2].answers2[language],
-          ],
-        },
+        { id: 3 },
+        { description: [text.QEA.Accordion[2].answers[language]] },
+        { description: [text.QEA.Accordion[2].answers2[language]] },
       ],
     },
+    //4
     {
       Q: text.QEA.Accordion[3].questions[language],
       A: [
@@ -56,40 +62,18 @@ const QeA = () => {
         },
       ],
     },
+    //5
     {
       Q: text.QEA.Accordion[4].questions[language],
       A: [
-        {
-          id: 5,
-          description: [
-            text.QEA.Accordion[4].answers[language],
-            text.QEA.Accordion[4].answers2[language],
-          ],
-          lista: [
-            "Londres (LHR) - Rio de Janeiro (GIG): British Airway | 11h40",
-            "Madrid (MAD) - Rio de Janeiro (GIG): Iberia | 10h50",
-            "Lisbon (LIS) - Rio de Janeiro (GIG): TAP |  09h55",
-            "Porto (OPO) - Rio de Janeiro (GIG): TAP | 10h20",
-            "Paris (CDG) - Rio de Janeiro (GIG): Air France | 11h20",
-            "Amsterdam (AMS) - Rio de Janeiro (GIG): KLM | 11h55",
-            "Munique (MUC) - Rio de Janeiro (GIG): Lufthansa | 11h50",
-            "Roma (FCO) - Rio de Janeiro (GIG): ITA Airways | 12h15",
-            "Dubai (DXB) - Rio de Janeiro (GIG): Emirates | 14h20",
-            "Panamá (PTY) - Rio de Janeiro (GIG): Copa Airlines | 07h05",
-            "Bogotá (BOG) -Rio de Janeiro (GIG): Avianca | 06h20",
-            "Lima (LIM) - Rio de Janeiro (GIG): Latam | 05h05",
-            "Nova Yorque (JFK) - Rio de Janeiro (GIG): American Airline e Delta | 09h45",
-            "Miami (MIA) - Rio de Janeiro (GIG): American Airline | 08h07",
-            "Atlanta (ATA) - Rio de Janeiro (GIG): Delta | 09h45",
-            "Huston (IAH) - Rio de Janeiro (GIG): United Airlines | 10h15",
-          ],
-          additionalInfo: [
-            text.QEA.Accordion[4].answers3[language],
-            text.QEA.Accordion[4].answers4[language],
-          ],
-        },
+        { id: 5 },
+        { description: [text.QEA.Accordion[4].answers[language]] },
+        { description: [text.QEA.Accordion[4].answers2[language]] },
+        { description: [text.QEA.Accordion[4].answers3[language]] },
+        { description: [text.QEA.Accordion[4].answers4[language]] },
       ],
     },
+    //6
     {
       Q: text.QEA.Accordion[5].questions[language],
       A: [
@@ -99,6 +83,7 @@ const QeA = () => {
         },
       ],
     },
+    //7
     {
       Q: text.QEA.Accordion[6].questions[language],
       A: [
@@ -108,17 +93,16 @@ const QeA = () => {
         },
       ],
     },
+    //8
     {
       Q: text.QEA.Accordion[7].questions[language],
       A: [
         {
-          description: [
-            text.QEA.Accordion[7].answers[language],
-            text.QEA.Accordion[7].answer1[language],
-            text.QEA.Accordion[7].answer2[language],
-          ],
           id: 8,
         },
+        { description: [text.QEA.Accordion[7].answers[language]] },
+        { description: [text.QEA.Accordion[7].answer1[language]] },
+        { description: [text.QEA.Accordion[7].answer2[language]] },
 
         {
           emprAir: [
@@ -136,21 +120,21 @@ const QeA = () => {
             text.QEA.Accordion[7].opcoesRioDeJaneiro[1][language],
           ],
         },
-        { additionalInfo: [text.QEA.Accordion[7].answer3[language]] },
+        { description: [text.QEA.Accordion[7].answer3[language]] },
       ],
     },
+    //9
     {
       Q: text.QEA.Accordion[8].questions[language],
       A: [
         {
           id: 9,
-          description: [
-            text.QEA.Accordion[8].answers[language],
-            text.QEA.Accordion[8].answers2[language],
-          ],
         },
+        { description: [text.QEA.Accordion[8].answers[language]] },
+        { description: [text.QEA.Accordion[8].answers2[language]] },
       ],
     },
+    //10
     {
       Q: text.QEA.Accordion[9].questions[language],
       A: [
@@ -160,6 +144,7 @@ const QeA = () => {
         },
       ],
     },
+    //11
     {
       Q: text.QEA.Accordion[10].questions[language],
       A: [
@@ -169,6 +154,7 @@ const QeA = () => {
         },
       ],
     },
+    //12
     {
       Q: text.QEA.Accordion[11].questions[language],
       A: [
@@ -178,34 +164,45 @@ const QeA = () => {
         },
       ],
     },
+    //13
     {
-      Q: text.QEA.Accordion[9].questions[language],
+      Q: text.QEA.Accordion[12].questions[language],
       A: [
         {
           id: 13,
-          description: [text.QEA.Accordion[9].answers[language]],
+          description: [text.QEA.Accordion[12].answers[language]],
         },
       ],
     },
+    //14
     {
       Q: text.QEA.Accordion[13].questions[language],
       A: [
         { description: text.QEA.Accordion[13].answers[language] },
+        { id: 14 },
+        { description: [text.QEA.Accordion[13].answer2[language]] },
         {
-          id: 14,
-          description: [text.QEA.Accordion[13].answer2[language]],
-          lista: [
-            text.QEA.Accordion[13].flightKLM[0][language],
-            text.QEA.Accordion[13].flightKLM[1][language],
-            text.QEA.Accordion[13].flightKLM[2][language],
-          ],
+          description: [text.QEA.Accordion[13].flightKLM[0][language]],
+        },
+        {
+          description: [text.QEA.Accordion[13].flightKLM[1][language]],
+        },
+        {
+          description: [text.QEA.Accordion[13].flightKLM[2][language]],
+        },
 
-          emprAir: [text.QEA.Accordion[13].answer3[language]],
-          additionalInfo: [
-            text.QEA.Accordion[13].flightLUFTHANSA1[0][language],
-            text.QEA.Accordion[13].flightLUFTHANSA1[1][language],
-            text.QEA.Accordion[13].flightLUFTHANSA1[2][language],
-          ],
+        { description: [text.QEA.Accordion[13].answer3[language]] },
+        {
+          description: [text.QEA.Accordion[13].flightLUFTHANSA1[0][language]],
+        },
+        {
+          description: [text.QEA.Accordion[13].flightLUFTHANSA1[1][language]],
+        },
+        {
+          description: [text.QEA.Accordion[13].flightLUFTHANSA1[2][language]],
+        },
+        {
+          description: [text.QEA.Accordion[13].answer4[language]],
         },
       ],
     },
